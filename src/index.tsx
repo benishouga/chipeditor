3a3c007968ba3bb22986db3ba26db07c220aaf90
benishouga <benishouga@gmail.com>
2020-01-21 21:24:33 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import { BoardEditor } from './BoardEditor';
 
 const rootElement = document.getElementById('root');
+
 ReactDOM.render(<BoardEditor />, rootElement);
