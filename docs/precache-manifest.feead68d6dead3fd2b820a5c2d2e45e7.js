a2341d1fad4c3314e96d03c95cdb484dfb543970
benishouga <benishouga@gmail.com>
2020-01-21 21:14:03 +0900

    Add docs

---
+++ docs/precache-manifest.feead68d6dead3fd2b820a5c2d2e45e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c1c4b3cd3d0d07d0ed",
+    "url": "/static/js/main.6f808b4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6ddf607e1d6f8291300",
+    "url": "/static/js/2.c4bc9804.chunk.js"
+  },
+  {
+    "revision": "33177b05f0f5e2156130641b80d0d1bc",
+    "url": "/index.html"
+  }
+];
